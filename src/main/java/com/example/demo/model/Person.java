b9fb894ca09537e137d3d0ba9572bb6f3f80356f
Gouiaa, Ramy <ramy.gouiaa@hs-ruhrwest.de>
2021-07-28 12:58:39 +0200

    adding person class

---
+++ src/main/java/com/example/demo/model/Person.java
@@ -0,0 +1,14 @@
+package com.example.demo.model;
+
+import java.util.UUID;
+
+public class Person {
+    private final UUID id;
+    private final String name;
+
+
+    public Person(UUID id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+}

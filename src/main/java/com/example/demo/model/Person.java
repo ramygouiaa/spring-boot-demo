c081044c69b666769fff2ba5ab557cc4c17b3d38
Gouiaa, Ramy <ramy.gouiaa@hs-ruhrwest.de>
2021-07-28 13:06:48 +0200

    adding PersonDao class

--- src/main/java/com/example/demo/model/Person.java
+++ src/main/java/com/example/demo/model/Person.java
@@ -11,4 +11,12 @@ public class Person {
         this.id = id;
         this.name = name;
     }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
